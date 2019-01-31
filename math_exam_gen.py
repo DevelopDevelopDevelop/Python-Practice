75b4aaea5481864e0cce3197613e60dbc7d0d415
DevelopDevelopDevelop <40976057+DevelopDevelopDevelop@users.noreply.github.com>
2019-01-30 21:15:56 -0500

    Update math_exam_gen.py
    Corrections to comments

--- math_exam_gen.py
+++ math_exam_gen.py
@@ -2,19 +2,19 @@
 import random
 import time
 start1 = time.time()
-# Generate 3 random values
-# 3 digits each
+
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems for Subtraction
+
+# Generate 3 random values
+# 3 digits each    
     x = random.randint(100,999)
     y = random.randint(100,999)
     z = random.randint(100,999)
     ans = (x-y-z)
     key.append(ans)
-    
+# Generate 5 problems for Subtraction    
     print(n,"\n   ",x)
     print("   ",y)
     print("  -",z)
@@ -26,24 +26,26 @@ i = range(0,5)
 for n in i:
     print(n+1, "= ", key[n])
 
+# Measure and show execution time of block
 stop = time.time()
 print("\n", stop-start1,"secs to exe\n\n")
 
 
 start = time.time()
-# Generate 3 random values
-# 3 digits each
+
 key = []
 i = range(1,6)
 for n in i:
     
-# Generate 5 problems for Addition
+# Generate 3 random values
+# 3 digits each
     x = random.randint(100,999)
     y = random.randint(100,999)
     z = random.randint(100,999)
     ans = (x+y+z)
     key.append(ans)
-    
+
+# Generate 5 problems for Addition  
     print(n,"\n   ",x)
     print("   ",y)
     print("  +",z)
@@ -65,15 +67,14 @@ key = []
 i = range(1,6)
 for n in i:
     
-# Generate 5 problems for Multiplication
+# Generate random values for x and y
     x = random.randint(100,999)
     y = random.randint(10,99)
    
     ans = (x*y)
     key.append(ans)
-    
-    print(n,"\n   ",x)
-   
+# Generate 5 problems for Multiplication    
+    print(n,"\n   ",x)   
     print("   *",y)
     print("  ------\n")
     
@@ -92,19 +93,17 @@ start = time.time()
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems for Division
+# Generate random values for x and y
     x = random.randint(100,999)
-    y = random.randint(10,99)
-   
+    y = random.randint(10,99)   
     ans = (x/y)
     key.append(ans)
-    
+
+# Generate 5 problems for Division    
     print(n,"\n   ",x)
-   
-    
     print("  ------ =")
     print("    ",y, "\n")
+
 # Generate answer key
 print("\nAnswer Key:\n")    
 i = range(0,5)
@@ -116,19 +115,19 @@ print("\n", stop-start,"secs to exe\n\n")
 
 
 start = time.time()
-# Generate 3 random values
-# 2 digits each
+
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems
+# Generate 3 random values
+# 2 digits each    
     x = random.randint(10,99)
     y = random.randint(10,99)
     z = random.randint(10,99)
     ans = (x+y-z)
     key.append(ans)
-    
+
+# Generate 5 problems    
     print(n,"\n  ",x, "+", y, "-", z, "=\n")
    
 # Generate answer key
@@ -142,19 +141,19 @@ print("\n", stop-start,"secs to exe\n\n")
 
 
 start = time.time()
-# Generate 3 random values
+
 
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems
+# Generate 3 random values    
     x = random.randint(10,99)
     y = random.randint(1,12)
     z = random.randint(1,10)
     ans = (x-y*z)
     key.append(ans)
-    
+
+# Generate 5 problems    
     print(n,"\n   ",x, "-", y, "*", z, "=\n")
    
 # Generate answer key
@@ -168,19 +167,19 @@ print("\n", stop-start,"secs to exe\n\n")
 
 
 start = time.time()
-# Generate 3 random values
 
 key = []
 i = range(1,6)
 for n in i:
     
-# Generate 5 problems
+# Generate 3 random values
     x = random.randint(10,99)
     y = random.randint(1,12)
     z = random.randint(1,10)
     ans = (x-y/z)
     key.append(ans)
-    
+
+# Generate 5 problems    
     print(n,"\n   ",x, "-", y, "/", z, "=\n")
    
 # Generate answer key
@@ -198,12 +197,12 @@ start = time.time()
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems
     d = random.randint(1,100)
     r = d/2
     ans = 2*3.14*r
     key.append(ans)
+
+# Generate 5 problems
     print(n,"\n     A Circle with a Diameter of", d, "\n  has a Circumference of\n")
    
 # Generate answer key
@@ -221,12 +220,12 @@ start = time.time()
 key = []
 i = range(1,6)
 for n in i:
-    
-# Generate 5 problems
     d = random.randint(1,100)
     r = d/2
     ans = 3.14*r**2
     key.append(ans)
+
+# Generate 5 problems
     print(n,"\n   A Circle with a Diameter of", d, "has an\n Area of\n")
    
 # Generate answer key
@@ -237,4 +236,6 @@ for n in i:
 
 stop = time.time()
 print("\n", stop-start,"secs to exe\n")
+
+# Measure and show total execution time
 print(stop-start1, 'total secs to exe')
