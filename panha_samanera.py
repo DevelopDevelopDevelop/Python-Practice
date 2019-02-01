30a2bff747372e92c217f4c6a4d70c70e8e80fda
DevelopDevelopDevelop <40976057+DevelopDevelopDevelop@users.noreply.github.com>
2019-02-01 00:16:44 -0500

    Create panha_samanera.py
    Part of a collection of study materials for the Theravada Buddhist novice monk

---
+++ panha_samanera.py
@@ -0,0 +1,36 @@
+# Questions from Buddha to Sopaka
+# Things to be known by a novice monk
+
+num1 = ("\nWhat is One?\nAll beings exist on food. ")
+num2 = ("\nWhat is Two?\nName and form. ")
+num3 = ("\nWhat is Three?\nThree kinds of feeling. ")
+num4 = ("\nWhat is Four?\nFour Noble Truths.")
+num5 = ("\nWhat is Five?\nFive aggregates subject to grasping.")
+num6 = ("\nWhat is Six?\nInternal six-fold base.")
+num7 = ("\nWhat is Seven?\nSeven Factors of Enlightenment.")
+num8 = ("\nWhat is Eight?\nThe Noble Eightfold Path")
+num9 = ("\nWhat is Nine?\nNine abodes of beings.")
+num10 = ("\nWhat is Ten?\nHe that is endowed with the ten attributes is called an Arahant.")
+
+number = int(input("Enter a number from 1 to 10: \n"))
+
+if number == 1:
+    print(num1)
+if number == 2:
+    print(num2)
+if number == 3:
+    print(num3)
+if number == 4:
+    print(num4)
+if number == 5:
+    print(num5)
+if number == 6:
+    print(num6)
+if number == 7:
+    print(num7)
+if number == 8:
+    print(num8)
+if number == 9:
+    print(num9)
+if number == 10:
+    print(num10)
